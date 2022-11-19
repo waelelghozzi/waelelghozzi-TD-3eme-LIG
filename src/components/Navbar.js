fcb73c2485218f033718ea990d47560b5b597eab
El Ghozzi Wael <waelghozzi232@gmail.com>
2022-11-19 17:56:56 +0100

    new update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,21 @@
 import React, { Component } from 'react';
 
 import styles from "./css/Navbar.module.css";
+import logo from './images/580b57fcd9996e24bc43c529.png';
 
 export default class Navbar extends Component {
   render() {
- let navigation=['home','tv shows','movies','popular','my list'];
+ //let navigation=['home','tv shows','movies','popular','my list'];
     return (
       <div className={styles.Navbar} >
-        <img src='images/580b57fcd9996e24bc43c529.png' alt='' className={styles.logo}/>
+        <img src={logo} alt='' className={styles.logo}/>
         <div className={styles.navlist}>
             {/*{navigation.map( item =>{<li>{item}</li>})}*/}
-            <div>home</div>
-            <div>tv shows</div>
-            <div>movies</div>
-            <div>popular</div>
-            <div>my list</div>
+            <div>Home</div>
+            <div>Tv shows</div>
+            <div>Movies</div>
+            <div>Popular</div>
+            <div>My list</div>
         </div>
       </div>
     )
